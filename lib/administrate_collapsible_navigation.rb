7e71f7d0962085a9f1d2d0c05ef36a099f4f16a7
Andrei Bondarev <andrei.bondarev13@gmail.com>
2019-03-23 00:11:05 -0400

    Working version

--- lib/administrate_collapsible_navigation.rb
+++ lib/administrate_collapsible_navigation.rb
@@ -2,5 +2,8 @@ require 'administrate_collapsible_navigation/version'
 
 module AdministrateCollapsibleNavigation
   class Engine < ::Rails::Engine
+    initializer "administrate_collapsible_navigation.assets.precompile" do |app|
+      app.config.assets.precompile += %w( left_arrow.svg right_arrow.svg )
+    end
   end
 end
