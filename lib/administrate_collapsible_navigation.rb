9f878bb0c4910d2d02ba3911633234d7a9be60c8
Andrei Bondarev <andrei.bondarev13@gmail.com>
2019-03-23 15:58:55 -0400

    Move JS out to a dedicated file

--- lib/administrate_collapsible_navigation.rb
+++ lib/administrate_collapsible_navigation.rb
@@ -1,9 +1,14 @@
 require 'administrate_collapsible_navigation/version'
+require 'administrate/engine'
 
 module AdministrateCollapsibleNavigation
   class Engine < ::Rails::Engine
     initializer "administrate_collapsible_navigation.assets.precompile" do |app|
       app.config.assets.precompile += %w( menu_open.png menu_closed.png )
     end
+
+    Administrate::Engine.add_javascript(
+      'administrate_collapsible_navigation/application'
+    )
   end
 end
